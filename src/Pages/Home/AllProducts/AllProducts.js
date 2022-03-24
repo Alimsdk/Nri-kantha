4f677684ec6bab6518e4de0bf633289fd23c6e59
Alimsdk <alimsiddique04@gmail.com>
2022-03-24 17:59:56 +0600

    payment section updated

--- src/Pages/Home/AllProducts/AllProducts.js
+++ src/Pages/Home/AllProducts/AllProducts.js
@@ -11,6 +11,11 @@ const AllProducts = () => {
        }
        fetchProduct();
     },[])
+
+   if(!allProducts){
+       return <h2>loading...</h2>
+   }
+
     return (
         <div style={{backgroundColor:'#ffe7e763'}} className="mb-5 pb-5">
             <h3 className='text-2xl ml-3 md:ml-9 py-5'>All Products</h3>
--- src/Pages/Payment/Payment.js
+++ src/Pages/Payment/Payment.js
@@ -54,9 +54,9 @@ const Payment = () => {
                 <h3 className='text-lg' > মোট মূল্যের ৩০% অগ্রিম প্রদানের মাধ্যমে অর্ডার নিশ্চিত করুন !</h3>
                 <h4 className='mt-5'>আপনার ক্রয়কৃৎ পন্যের মোট মূল্য {finalTotalAmount} টাকার ৩০% অর্থাৎ <span className='bg-yellow-400'>{finalTotalAmount*0.3} টাকা</span> বিকাশের মাধ্যমে পরিশোধ করুন</h4>
                 <h4 className=" my-9">
-                   বিকাশে  <span className='bg-yellow-400'>01763572771</span> এই নম্বরে <span className='bg-yellow-400'>{finalTotalAmount*0.3} টাকা</span> সেন্ড মানি করে মেসেজে যে ট্রাঞ্জিকশন আইডি পাবেন সেটি প্রদান করুণ!
+                   বিকাশে  <span className='bg-yellow-400'>01763572771</span> এই নম্বরে <span className='bg-yellow-400'>{finalTotalAmount*0.3} টাকা</span> সেন্ড মানি করে মেসেজে যে ট্রাঞ্জিকশন আইডি পাবেন সেটি <br /> <br /> অথবা বিকাশ নাম্বারের শেষের ৩ নম্বর প্রদান করুণ!
                 </h4>
-             <input className="w-96 border-2 py-1.5 px-9 rounded text-left text-sm border-black" onBlur={(e)=>transictionId=e.target.value} type="text" placeholder= 'মেসেজে পাওয়া ট্রাঞ্জিকশন আইডি প্রদান করুন ' />
+             <input className="w-96 border-2 py-2 px-9 rounded text-left text-xs border-black" onBlur={(e)=>transictionId=e.target.value} type="text" placeholder= 'মেসেজে পাওয়া ট্রাঞ্জিকশন আইডি প্রদান করুন অথবা বিকাশ নাম্বারের শেষের ৩ নম্বর প্রদান করুণ! ' />
               <br /> <br />  <button className='bg-yellow-400 px-5' onClick={handleTransId}>অর্ডার নিশ্চিত করুন</button> <br /> <br />
               <h3 className='text-sm text-red-600'>অর্ডার করতে গিয়ে কোনো সমস্যার সম্মুখীন হলে 01614821673 নম্বরে যোগাযোগ করুন</h3>
             </div>
