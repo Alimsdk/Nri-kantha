ac6514a411fbae8eccd5888f2e462b9048bed384
Alimsdk <alimsiddique04@gmail.com>
2022-12-02 19:30:33 +0600

    server deployed

--- src/Pages/Home/ProductDetail/ProductDetail.js
+++ src/Pages/Home/ProductDetail/ProductDetail.js
@@ -20,7 +20,7 @@ const ProductDetail = () => {
 
 
     useEffect(()=>{
-       const url= `https://nameless-castle-34131.herokuapp.com/products/${id}`;
+       const url= `https://nrikantha.up.railway.app/products/${id}`;
        const fetchDetails=async()=>{
            
            const res=await fetch(url);
